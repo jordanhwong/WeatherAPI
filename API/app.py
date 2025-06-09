e1ab58085db42404ee7818a3c975bc06024b9131
Jordan Wong <jordanhwong1@gmail.com>
2025-06-09 16:23:55 -0700

    basic api functionality

--- API/app.py
+++ API/app.py
@@ -1,22 +1,28 @@
 import os
 import requests
 
+from flask import Flask, jsonify
 
 from dotenv import find_dotenv, load_dotenv
 
-dotenv_path = find_dotenv()
+app = Flask(__name__)
 
+dotenv_path = find_dotenv()
 if dotenv_path:
     load_dotenv(dotenv_path)
 
 API_KEY = os.getenv("API_KEY")
 print (API_KEY)
 
-url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Milpitas?key=" + API_KEY
-response = requests.get(url)
-if response.status_code == 200:
-    data = response.json()
-    print(data)
-else:
-    print(f"Error: {response.status_code}")
-    response.raise_for_status()
+    
+@app.route('/weather/<location>', methods=['GET'])
+def get_weather(location):
+
+    url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}?key={API_KEY}"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return jsonify(data)
+    else:
+        return jsonify({"error": "Failed to fetch weather data"}), response.status_code
